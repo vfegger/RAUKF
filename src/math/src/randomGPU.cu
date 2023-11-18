98de5541aa1972f83aede041341189afc1955a04
vfegger <vitor.egger@gmail.com>
2023-11-18 15:57:37 -0300

    Minor Commit - Minor Fix

--- src/math/src/randomGPU.cu
+++ src/math/src/randomGPU.cu
@@ -6,4 +6,5 @@ void RandomGPU::SampleNormal(double *v_o, int length, double mean, double sigma)
     curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_DEFAULT);
     curandSetStream(generator,cudaStreamDefault);
     curandGenerateNormalDouble(generator,v_o,length,mean,sigma);
+    curandDestroyGenerator(generator);
 }

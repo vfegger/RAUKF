94ff75e909df403def2330ddf382a5c335d3cdb3
vfegger <vitor.egger@gmail.com>
2023-11-30 16:20:31 -0300

    Minor Commit - Minor Fix

--- src/model/src/aem.cpp
+++ src/model/src/aem.cpp
@@ -2,8 +2,6 @@
 
 #include <fstream>
 
-int it = 0;
-
 void AEM::SetMemory(int Lcs, int Lcm, int Lrs, int Lrm, Type type)
 {
     errorState.alloc(Lrs);
@@ -74,32 +72,6 @@ void AEM::CorrectEvaluation(Measure *pmeasure, Data *pstate, Type type)
     Math::Add(pmeasure->GetMeasureCovariancePointer(), covarMeasure, L * L, type);
 }
 
-void PrintMatrix(std::string name, Pointer<double> mat, int lengthI, int lengthJ, Type type)
-{
-    if (type == Type::GPU)
-    {
-        cudaDeviceSynchronize();
-        mat.copyDev2Host(lengthI * lengthJ);
-    }
-    double *p = mat.host();
-
-    std::ofstream fp;
-    fp.open(name + std::to_string(it) + ".csv");
-    for (int i = 0; i < lengthI; ++i)
-    {
-        for (int j = 0; j < lengthJ; ++j)
-        {
-            fp << p[j * lengthI + i];
-            if (j < lengthJ)
-            {
-                fp << ",";
-            }
-        }
-        fp << "\n";
-    }
-    fp.close();
-}
-
 Pointer<double> AEM::Evolve(Data *pstate, ExecutionType execType, Type type)
 {
     unsigned Lr = prState->GetStateLength();
@@ -135,9 +107,6 @@ Pointer<double> AEM::Evolve(Data *pstate, ExecutionType execType, Type type)
     Math::Mean(errorState, samplesState + Lr * N, Lr, N, type);
     Math::Iterate(Math::Sub, samplesState, errorState, Lr, N, Lr, 0, Lr * N, 0, type);
     Math::MatMulNT(0.0, covarState, 1.0 / (N - 1.0), samplesState + Lr * N, samplesState + Lr * N, Lr, N, Lr, type);
-
-    //PrintMatrix("EvolveError", errorState, Lr, 1, type);
-    //PrintMatrix("EvolveCovar", covarState, Lr, Lr, type);
 
     return reducedModel->Evolve(pstate, execType, type);
 }
@@ -182,10 +151,6 @@ Pointer<double> AEM::Evaluate(Measure *pmeasure, Data *pstate, ExecutionType exe
     Math::Mean(errorMeasure, samplesMeasure + Lrm * N, Lrm, N, type);
     Math::Iterate(Math::Sub, samplesMeasure, errorMeasure, Lrm, N, Lrm, 0, Lrm * N, 0, type);
     Math::MatMulNT(0.0, covarMeasure, 1.0 / (N - 1.0), samplesMeasure + Lrm * N, samplesMeasure + Lrm * N, Lrm, N, Lrm, type);
-
-    //PrintMatrix("EvaluateError", errorMeasure, Lrm, 1, type);
-    //PrintMatrix("EvaluateCovar", covarMeasure, Lrm, Lrm, type);
-    it++;
 
     return reducedModel->Evaluate(pmeasure, pstate, execType, type);
 }

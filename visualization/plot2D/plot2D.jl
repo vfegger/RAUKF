abac80a6dc508cd40d19c75a8c9cb10a4b1fd806
vfegger <vitor.egger@gmail.com>
2024-10-14 20:55:26 -0300

    Minor Commit - Update Plot2D

--- visualization/plot2D/plot2D.jl
+++ visualization/plot2D/plot2D.jl
@@ -53,19 +53,55 @@ function getData()
     end
 end
 
-function printGraphs()
-
+function printProfiles(case, t)
+    @assert t >= 1
+    @assert t <= Lt
     dx2 = Sx / Lx / 2
     dy2 = Sy / Ly / 2
     amp = 1e3
 
     x = range(0 + dx2, Sx - dx2, Lx)
     y = range(0 + dy2, Sy - dy2, Ly)
-    t = 100
     zT = @view(dataValues[:T][:, :, t])
+    zcT = @view(dataValues[:cT][:, :, t])
+    zQ = @view(dataValues[:Q][:, :, t])
+    zcQ = @view(dataValues[:cQ][:, :, t])
+    zTm = @view(dataValues[:Tm][:, :, t])
+    zcTm = @view(dataValues[:cTm][:, :, t])
+    zTsN = @view(dataValues[:TsN][:, :, t])
+    zTs = @view(dataValues[:Ts][:, :, t])
+    zQs = @view(dataValues[:Qs][:, :, t])
+
     colgrad = cgrad(:thermal, rev=false)
 
-    pltRes = heatmap(x, y, zT, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal,title="Temperature",xlabel="X [m]",ylabel="Y [m]",colorbar_title ="Temperature [K]",dpi=1000)
+    plt_T_Profile = heatmap(x, y, zT, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Temperature", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Temperature [K]", dpi=1000)
+    savefig(plt_T_Profile, joinpath(imagePath, case, "TemperatureProfile_" * string(t) * ".pdf"))
+    
+    plt_cT_Profile = heatmap(x, y, zcT, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_cT_Profile, joinpath(imagePath, case, "TemperatureCovarianceProfile_" * string(t) * ".pdf"))
+    
+    plt_Q_Profile = heatmap(x, y, zQ, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_Q_Profile, joinpath(imagePath, case, "HeatFluxProfile_" * string(t) * ".pdf"))
+    
+    plt_cQ_Profile = heatmap(x, y, zcQ, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_cQ_Profile, joinpath(imagePath, case, "HeatFluxCovarianceProfile_" * string(t) * ".pdf"))
+    
+    plt_Tm_Profile = heatmap(x, y, zTm, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_Tm_Profile, joinpath(imagePath, case, "ObservedTemperatureProfile_" * string(t) * ".pdf"))
+    
+    plt_cTm_Profile = heatmap(x, y, zcTm, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_cTm_Profile, joinpath(imagePath, case, "ObservedTemperatureCovarianceProfile_" * string(t) * ".pdf"))
+    
+    plt_TsN_Profile = heatmap(x, y, zTsN, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_TsN_Profile, joinpath(imagePath, case, "NoisyMeasureTemperatureProfile_" * string(t) * ".pdf"))
+    
+    plt_Ts_Profile = heatmap(x, y, zTs, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_Ts_Profile, joinpath(imagePath, case, "MeasureTemperatureProfile_" * string(t) * ".pdf"))
+    
+    plt_Qs_Profile = heatmap(x, y, zQs, xlims=(0, Sx), ylims=(0, Sy), yflip=false, c=colgrad, aspect_ratio=:equal, title="Heat Flux", xlabel="X [m]", ylabel="Y [m]", colorbar_title="Heat Flux [W/m^2]", dpi=1000)
+    savefig(plt_Qs_Profile, joinpath(imagePath, case, "SimulatedHeatFluxProfile_" * string(t) * ".pdf"))
+
+    display(plt_T_Profile)
 end
 
 getData()

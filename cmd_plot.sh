2f1f690518ed1da9d6a2e824fcf97681844ef513
vfegger <vitor.egger@gmail.com>
2024-11-02 05:47:38 -0300

    Minor Commit - Update Commands

---
+++ cmd_plot.sh
@@ -0,0 +1,6 @@
+julia visualization/plot2D/plot2D.jl Simulation1 0
+julia visualization/plot2D/plot2D.jl Simulation2 0
+julia visualization/plot2D/plot2D.jl Simulation3 0
+julia visualization/plot2D/plot2D.jl Simulation4 0
+julia visualization/plot2D/plot2D.jl Experimental2_h0_amp5e3_o0 1
+julia visualization/plot2D/plot2D.jl Experimental3_h0_amp5e3_o0 1
